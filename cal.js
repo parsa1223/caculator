c31e9eb846550be263addfad316240e7a632f56d
Parsa Arababadi <arababadiparsa@gmail.com>
2024-03-14 12:34:50 +0330

    calculator

---
+++ cal.js
@@ -0,0 +1,16 @@
+let namayeshgar=document.getElementById('namayeshgar');
+function safheNamayesh(value){
+    namayeshgar.value += value;
+
+}
+function pakKardan(){
+    namayeshgar.value="";
+
+}
+function mohasebe (){
+try{
+    namayeshgar.value=eval(namayeshgar.value);
+}catch(error) {
+    namayeshgar.value='Error'
+}
+}
